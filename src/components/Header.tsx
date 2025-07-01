f837c31be3d8ab195c5dbc90bb362a76bdc6f62b
Code-max <145682294+isaacodex-max@users.noreply.github.com>
2025-07-01 11:15:05 +0100

    Initial project commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage, onNavigate }) => {
 
   return (
     <header className="bg-white shadow-lg fixed top-0 left-0 right-0 z-50">
-      <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <div className="flex items-center cursor-pointer" onClick={() => onNavigate('home')}>
