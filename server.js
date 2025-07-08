2dacfb9e0dc427da8c5f931ccb83aa9639aaf010
Code-max <145682294+isaacodex-max@users.noreply.github.com>
2025-07-08 16:43:15 +0100

    application form and server.js

--- server.js
+++ server.js
@@ -63,4 +63,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
